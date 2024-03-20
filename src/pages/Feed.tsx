9c053ebb35b287347dcd2fe7f6af8ee6c7a2e6e3
coder7475 <robiulhossain7475@gmail.com>
2024-03-21 00:07:21 +0600

    feat: use query in a component

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,10 +1,28 @@
+import { useGetPokemonByNameQuery } from "../redux/features/api/baseApi";
 
 const Feed = () => {
-    return (
-        <main>
-           <h1 className="text-5xl">News Feed</h1>
-        </main>
-    );
+  const {
+    data: pokeData,
+    isLoading,
+    // isError,
+    error,
+  } = useGetPokemonByNameQuery("bulbasaur");
+
+  return (
+    <main>
+      <h1 className="text-5xl">Pokemon Data</h1>
+      {error ? (
+        <>Ho no, their is no data for bulbasaur</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : pokeData ? (
+        <>
+          <h3>{pokeData?.species?.name}</h3>
+          <img src={pokeData.sprites.front_shiny} alt={pokeData.species.name} />
+        </>
+      ) : null}
+    </main>
+  );
 };
 
 export default Feed;
