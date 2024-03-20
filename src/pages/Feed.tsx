fda74643540faf9daebeb4d0696a276b5326807a
coder7475 <robiulhossain7475@gmail.com>
2024-03-21 00:10:12 +0600

    feat: center the data

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -10,16 +10,16 @@ const Feed = () => {
 
   return (
     <main>
-      <h1 className="text-5xl">Pokemon Data</h1>
+      <h1 className="font-medium">Pokemon Data</h1>
       {error ? (
         <>Ho no, their is no data for bulbasaur</>
       ) : isLoading ? (
         <>Loading...</>
       ) : pokeData ? (
-        <>
+        <div className="flex justify-center items-center">
           <h3>{pokeData?.species?.name}</h3>
           <img src={pokeData.sprites.front_shiny} alt={pokeData.species.name} />
-        </>
+        </div>
       ) : null}
     </main>
   );
