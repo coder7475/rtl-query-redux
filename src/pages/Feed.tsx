2e7931aa890c8bbd72e78935e18cb1a7da811c6e
coder7475 <robiulhossain7475@gmail.com>
2024-03-22 19:09:15 +0600

    feat: show loading and when somethid went wrong in feed

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -2,19 +2,28 @@ import { useGetPostsQuery } from "../redux/features/api/baseApi";
 import { Post } from "../redux/interfaces/post";
 
 const Feed = () => {
-  const { data:posts, isLoading, error, isError  } = useGetPostsQuery();
-  
+  const { data: posts, isLoading, error, isError } = useGetPostsQuery("");
+
+  if (isLoading) {
+    return <p className="text-5xl text-zinc-300">Loading...</p>
+  }
+
+  if (!isLoading && isError) {
+    return <p className="text-5xl text-zinc-300">SomeThing Went wrong</p>
+  }
+
   return (
     <main>
       <h1 className="font-black text-4xl">News Feed</h1>
-      {
-        posts?.map((post: Post) => (
-          <section key={post?.id} className="w-1/2 mx-auto my-5">
-            <h2 className="text-xl font-semibold space-y-2">{post?.title}</h2>
-            <p>{post?.body}</p>
-          </section>
-        ))
-      }
+      {posts?.map((post: Post) => (
+        <section key={post?.id} className="w-1/2 mx-auto my-5">
+          <h2 className="text-xl font-semibold space-y-2">
+            {post?.id} {post?.title}
+          </h2>
+          <p>By {post?.userId}</p>
+          <p>{post?.body}</p>
+        </section>
+      ))}
     </main>
   );
 };
