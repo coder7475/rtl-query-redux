c763df1ed5c81cc4ff83ea1bd21c38c759f79582
coder7475 <robiulhossain7475@gmail.com>
2024-03-22 19:00:20 +0600

    feat: show feed to homepage

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,14 +1,17 @@
 import { useGetPostsQuery } from "../redux/features/api/baseApi";
+import { Post } from "../redux/interfaces/post";
 
 const Feed = () => {
-  const { data:posts, isLoading,   } = useGetPostsQuery("");
+  const { data:posts, isLoading, error, isError  } = useGetPostsQuery();
   
   return (
     <main>
+      <h1 className="font-black text-4xl">News Feed</h1>
       {
-        posts?.map((post) => (
-          <section key={post?.id}>
-
+        posts?.map((post: Post) => (
+          <section key={post?.id} className="w-1/2 mx-auto my-5">
+            <h2 className="text-xl font-semibold space-y-2">{post?.title}</h2>
+            <p>{post?.body}</p>
           </section>
         ))
       }
